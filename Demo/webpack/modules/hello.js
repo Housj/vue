469e2dc6e54a3892b9a6f62b6ba81443e1511ea8
housj <1104790782@qq.com>
2020-05-10 15:07:26 +0800

    add

---
+++ Demo/webpack/modules/hello.js
@@ -0,0 +1,3 @@
+exports.say = function () {
+    document.write("<div>Hello Webpack</div>")
+}
---
+++ Demo/webpack/webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    entry: "./modules/main.js",
+    output: {
+        // path: "",
+        filename: "./js/bundle.js"
+    }
+    // module: {
+    //     loaders: [
+    //         {test: /\.js$/, loader: ""}
+    //     ]
+    // },
+    // plugins: {},
+    // resolve: {},
+    // watch: true 热部署
+}
